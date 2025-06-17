90a1a06cd5a21dc2c3d5e9a0445c2d6a3955b627
Husted42 <alx2608@gmail.com>
2025-06-17 17:47:30 +0200

    ..

--- test_01.py
+++ test_01.py
@@ -1,6 +1,7 @@
 import serial
 import argparse
 import threading
+import json
 
 def read_serial():
     while True:
@@ -9,6 +10,21 @@ def read_serial():
             print(f"Received: {data}", end='')
 
 def main():
+    json_command = json.dumps({
+        "T": 1051,
+        "x": 373.8055574,
+        "y": 287.1348881,
+        "z": 112.6639825,
+        "b": 0.655009796,
+        "s": 0.813009818,
+        "e": 0.859029241,
+        "t": 3.136990711,
+        "torB": 80,
+        "torS": 200,
+        "torE": -116,
+        "torH": 0
+    })
+
     global ser
     parser = argparse.ArgumentParser(description='Serial JSON Communication')
     parser.add_argument('port', type=str, help='Serial port name (e.g., COM1 or /dev/ttyUSB0)')
